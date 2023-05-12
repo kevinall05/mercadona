9ba312e62f8d83e55d62aad5a1709fa47f31d085
Kevin Alld <allemandkevin1@gmail.com>
2023-05-12 16:54:49 +0200

    Fix affichage promotions catégories

--- src/Controller/CategoriesController.php
+++ src/Controller/CategoriesController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\Categories;
 use App\Repository\ProductsRepository;
+use App\Repository\PromotionsRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -13,19 +14,19 @@ use Symfony\Component\Routing\Annotation\Route;
 class CategoriesController extends AbstractController
 {
     #[Route('/{slug}', name: 'list')]
-    public function list(Categories $category, ProductsRepository $productsRepository, Request $request): Response
+    public function list(Categories $category, ProductsRepository $productsRepository,PromotionsRepository $promotionsRepository, Request $request): Response
     {
         //On va chercher le numéro de page dans l'url
         $page = $request->query->getInt('page', 1);
 
         //On va chercher la liste des produits de la catégorie
         $products = $productsRepository->findProductsPaginated($page, $category->getSlug(), 4);
+        $promotions = $promotionsRepository->findAll();
 
-        return $this->render('categories/list.html.twig', compact('category', 'products'));
-        // Syntaxe alternative
-        // return $this->render('categories/list.html.twig', [
-        //     'category' => $category,
-        //     'products' => $products
-        // ]);
+        return $this->render('categories/list.html.twig', [
+            'category' => $category,
+            'products' => $products,
+            'promotions' => $promotions,
+        ]);
     }
 }
