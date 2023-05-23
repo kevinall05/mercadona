8d5766cc9cfcd363d5cf22414b60bab38c5ff3cd
Kevin Alld <allemandkevin1@gmail.com>
2023-05-23 19:07:10 +0200

    fix affichage

--- src/Controller/ProductsController.php
+++ src/Controller/ProductsController.php
@@ -3,6 +3,9 @@
 namespace App\Controller;
 
 use App\Entity\Products;
+use App\Repository\CategoriesRepository;
+use App\Repository\ProductsRepository;
+use App\Repository\PromotionsRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,9 +14,18 @@ use Symfony\Component\Routing\Annotation\Route;
 class ProductsController extends AbstractController
 {
     #[Route('/', name: 'index')]
-    public function index(): Response
+    public function index(CategoriesRepository $categoriesRepository, ProductsRepository $productsRepository, PromotionsRepository $promotionsRepository): Response
     {
-        return $this->render('products/index.html.twig');
+        $categories = $categoriesRepository->findBy([], ['categoryOrder' => 'asc']);
+        $products = $productsRepository->findAll();
+        $promotions = $promotionsRepository->findAll();
+
+
+        return $this->render('products/index.html.twig', [
+            'categories' => $categories,
+            'products' => $products,
+            'promotions' => $promotions,
+        ]);
     }
 
     #[Route('/{slug}', name: 'details')]
